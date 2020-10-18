5f8e65b8cec5d4e257e7b12f61e12c3a4971d72d
Simon Shine <shreddedglory@gmail.com>
2020-10-18 10:20:02 +0200

    Request, Response

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,36 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Main where
 
+import Data.Aeson
+import Data.Text (Text)
+import GHC.Generics (Generic)
+import Servant
+
+type SwordAPI = "compile" :> ReqBody '[JSON] CompileRequest :> Post '[JSON] CompileResponse
+           :<|> "ping" :> Get '[JSON] PingResponse
+
+data CompileRequest = CompileRequest
+  { contract   :: Text
+  , blockchain :: Text
+  } deriving (Generic)
+
+instance FromJSON CompileRequest
+
+data CompileResponse = CompileResponse
+  { bytecode :: Text
+  , parties  :: Int
+  } deriving (Generic)
+
+instance ToJSON CompileResponse
+
+data PingResponse = PingResponse
+  { running :: Bool
+  } deriving (Generic)
+
+instance ToJSON PingResponse
+
 main :: IO ()
-main = putStrLn "Sword-REST"
+main = putStrLn "Sword+REST"
